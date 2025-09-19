f833dadf8a1f5b8f7597b9be3ee2c8c09cc42d41
Muyassar-Mamdoh-Salem <mesoana200155@gmail.com>
2025-09-19 16:37:39 +0300

    seo6

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className='bg-[#0A0F1F]'>
             <div className="h-[1px] bg-gray-600"></div>
             <div className="container mx-auto px-[5%] text-gray-500 py-10 text-center">
-                <span className='text-[10px] md:text-[13px]'>@ 2025 - All Rights Reserved Web Design And Development - Muyassar Mamdoh</span>
+                <span className='text-[12px] md:text-[14px]'>@ 2025 - All Rights Reserved Web Design And Development - Muyassar Mamdoh</span>
             </div>
         </footer>
     )
